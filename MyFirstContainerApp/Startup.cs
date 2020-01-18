3569edebb35a08ac42073e596032e44b04ef2af1
Chris Magill <chris.magill87@gmail.com>
2020-01-18 15:18:32 +0000

    Automated build?

--- MyFirstContainerApp/Startup.cs
+++ MyFirstContainerApp/Startup.cs
@@ -17,6 +17,7 @@ namespace MyFirstContainerApp
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
